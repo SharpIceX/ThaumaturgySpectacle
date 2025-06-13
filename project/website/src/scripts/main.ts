f6f67d950c40042f56f1340e2231f52502aba6c7
SharpIce <VupRbl@163.com>
2025-06-14 03:50:52 +0800

    初始提交 5

--- project/website/src/scripts/main.ts
+++ project/website/src/scripts/main.ts
@@ -12,3 +12,11 @@ import { OverlayScrollbars } from 'overlayscrollbars';
 window.osInstance = OverlayScrollbars(document.body, {
 	//
 });
+
+window.addEventListener('load', () => {
+	// 当页面加载完成后，移除 FOUC 和预加载样式
+	const styles = document.querySelectorAll('head style[data-fouc-fix], head style[data-pre-fix]');
+	styles.forEach(style => {
+		style.remove();
+	});
+});
--- project/website/uno.config.ts
+++ project/website/uno.config.ts
@@ -2,6 +2,13 @@ import presetNord from 'unocss-preset-nord';
 import { defineConfig, presetWind3, transformerDirectives, transformerVariantGroup } from 'unocss';
 
 export default defineConfig({
+	theme: {
+		extend: {
+			colors: {
+				theme: '#679DE3',
+			},
+		},
+	},
 	presets: [presetNord(), presetWind3()],
 	transformers: [transformerDirectives(), transformerVariantGroup()],
 });
